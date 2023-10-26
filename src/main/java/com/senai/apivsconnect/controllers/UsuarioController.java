16a4e486c9837b874b34c71d534a39fb38bbb306
Vinicius <viniciuszaneti.vz@gmail.com>
2023-10-26 20:06:17 -0300

    subindo atualizacoes da api

--- src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -1,14 +1,14 @@
 package com.senai.apivsconnect.controllers;
 
+import com.senai.apivsconnect.dtos.UsuarioDto;
 import com.senai.apivsconnect.models.UsuarioModel;
 import com.senai.apivsconnect.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,5 +35,17 @@ public class UsuarioController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(usuarioBuscado.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> cadastrarUsuario(@RequestBody @Valid UsuarioDto usuarioDto){
+        if (usuarioRepository.findByEmail(usuarioDto.email()) != null) {
+//            Nao pode cadastrar
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Esse email ja esta cadastrado!");
+        }
+        UsuarioModel usuario = new UsuarioModel();
+        BeanUtils.copyProperties(usuarioDto,usuario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
     }
 }
